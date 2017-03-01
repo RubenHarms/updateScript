dee1a09be6223f702058e506abd4bbe217f9864e
Ruben Harms <postbus@rubenharms.nl>
2017-03-01 12:28:54 +0100

    Added file structure

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pushd `dirname $0` > /dev/null
+CURRENT_PATH=`pwd -P`
+popd > /dev/null
+sudo cp $CURRENT_PATH/bin/updateScript  /usr/bin/updateScript -f
+
+if ! [[ -f  /usr/include/updateScript ]]
+then
+	mkdir -p /usr/include/updateScript
+fi
+
+sudo cp $CURRENT_PATH/include/main  /usr/include/updateScript/main -f
+
